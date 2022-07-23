52b350db1c242929ee3aa5610b735807082c0417
ghaya-2001 <ghyayaderbali2001@gmail.com>
2022-07-23 16:27:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+/**content of express in app */
+const app = express();
+
+app.options( cors())
+/**server yekhm mrigl ou non  */
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
+
+/**importing the content of our .en */
+require('dotenv').config();
+
+const mongoString = process.env.DATABASE_URL
+
+/**connecting database to the server */
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+/**success or error msg */
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+
+/** import the routes file in index.js*/
+const routes = require('./routes/routes');
+
+app.use('/api', routes, cors())
